7a4d2d37984c091f9c1d420e60e21292e19c5b97
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-29 14:16:54 +0700

    .

--- quiz/api/instance.js
+++ quiz/api/instance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
     // baseURL: "http://localhost:3002/"
-    baseURL: "sytomquizapi.vercel.app"
+    baseURL: "https://sytomquizapi.vercel.app/"
 })
 export default instance;

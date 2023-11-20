9a12fd56be76674050fb40f2d80101482eb9a63b
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-20 15:04:25 +0700

    update baseURL

--- quiz/api/instance.js
+++ quiz/api/instance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
     // baseURL: "http://localhost:3002/"
-    baseURL: "https://quiz-api-pi.vercel.app/"
+    baseURL: "https://quiz-api-git-main-sytom123.vercel.app/"
 })
 export default instance;
